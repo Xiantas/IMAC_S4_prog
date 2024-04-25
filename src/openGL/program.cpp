e4681ebc33e47de24a46267d5dbbb6d8eab551eb
Xiantas <mat.moulat@gmail.com>
2024-04-26 01:08:14 +0200

    fixed compilation error

--- src/openGL/program.cpp
+++ src/openGL/program.cpp
@@ -2,6 +2,7 @@
 #include <glad/glad.h>
 #include <iostream>
 #include <string>
+#include <vector>
 #include "file_utils.h"
 
 GLshader::GLshader(GLuint shaderType) : address(glCreateShader(shaderType)), shaderType(shaderType) {}
