82e6a64cfa1ac12f5b3ca8251bc7310360834541
Lucieguerin431 <lucie.guerin@edu.univ-eiffel.fr>
2024-04-26 00:29:12 +0200

    Little changes

--- src/maths.h
+++ src/maths.h
@@ -9,7 +9,6 @@ namespace maths {
     double gammaRandom(int alpha, double theta);
     double cauchyRandom(double location, double scale);
     int poissonRandom(double lambda);
-    
 }
 
 #endif // MATHS_H
